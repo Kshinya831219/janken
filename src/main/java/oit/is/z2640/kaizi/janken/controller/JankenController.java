76c9d2789ca7bc851bbde4c95555a27bcb832e5b
Kshinya831219 <e1b22036@oit.ac.jp>
2024-10-03 14:10:46 +0900

    CPUが手をランダムに出すように変更

--- src/main/java/oit/is/z2640/kaizi/janken/controller/JankenController.java
+++ src/main/java/oit/is/z2640/kaizi/janken/controller/JankenController.java
@@ -7,6 +7,8 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import oit.is.z2640.kaizi.janken.model.Janken;
+
 @Controller
 public class JankenController {
 
@@ -14,6 +16,17 @@ public class JankenController {
   public String janken(@RequestParam(name = "name", required = false) String userName, ModelMap model) {
 
     model.addAttribute("name", userName);
+    return "janken.html";
+  }
+
+  @GetMapping("/jankengame")
+  public String jankengame(@RequestParam String userhand, ModelMap model) {
+    Janken game = new Janken(userhand);
+    model.addAttribute("hand",game);
+    model.addAttribute("userhand", game.getUserHand());
+    model.addAttribute("cpuHand", game.getCpuHand());
+    model.addAttribute("result", game.getResult());
+
     return "janken.html";
   }
 
