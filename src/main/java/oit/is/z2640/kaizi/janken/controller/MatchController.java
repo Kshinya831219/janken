6eaf5f2ab76bba838e60290e508558f051f991e9
Kshinya831219 <e1b22036@oit.ac.jp>
2024-10-20 17:37:32 +0900

    試合を行い結果をデータベースに登録

--- src/main/java/oit/is/z2640/kaizi/janken/controller/MatchController.java
+++ src/main/java/oit/is/z2640/kaizi/janken/controller/MatchController.java
@@ -14,6 +14,7 @@ import oit.is.z2640.kaizi.janken.model.Janken;
 import oit.is.z2640.kaizi.janken.model.MatchMapper;
 import oit.is.z2640.kaizi.janken.model.User;
 import oit.is.z2640.kaizi.janken.model.UserMapper;
+import oit.is.z2640.kaizi.janken.model.Match;
 
 @Controller
 @RequestMapping("/match")
@@ -38,14 +39,26 @@ public class MatchController {
     return "match.html";
   }
 
-  @PostMapping("/jankengame")
-  public String jankengame(@RequestParam String userhand, ModelMap model) {
+  @GetMapping("/fight")
+  public String fight(@RequestParam String userhand, @RequestParam int opponentId, ModelMap model, Principal prin) {
 
     Janken game = new Janken(userhand);
-
     model.addAttribute("userhand", game.getUserHand());
     model.addAttribute("cpuHand", game.getCpuHand());
     model.addAttribute("result", game.getResult());
+    String loginUser = prin.getName();
+    User user = userMapper.findByName(loginUser);
+
+    Match match = new Match();
+
+    match.setUser1(user.getId());
+    match.setUser2(opponentId);
+    match.setUser1Hand(userhand);
+    match.setUser2Hand(game.getCpuHand());
+    matchMapper.insertMatch(match);
+    User opponent = userMapper.selectById(opponentId);
+    model.addAttribute("opponent", opponent);
+    model.addAttribute("loginUser", loginUser);
 
     return "match.html";
   }
--- src/main/java/oit/is/z2640/kaizi/janken/model/UserMapper.java
+++ src/main/java/oit/is/z2640/kaizi/janken/model/UserMapper.java
@@ -1,13 +1,11 @@
 package oit.is.z2640.kaizi.janken.model;
 
-
 import java.util.ArrayList;
 
 import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Options;
 import org.apache.ibatis.annotations.Select;
-
 
 @Mapper
 public interface UserMapper {
@@ -17,5 +15,8 @@ public interface UserMapper {
 
   @Select("SELECT * FROM users WHERE id = #{id}")
   User selectById(int id);
+
+  @Select("SELECT * FROM users WHERE name = #{name}")
+  User findByName(String name);
 
 }
